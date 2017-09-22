d21295dbfae38285bfb5a3fd855ef3189f03cf53
Levi Broderick <levib@microsoft.com>
2017-09-22 15:29:35 -0700

    Experiment with a potentially faster encoder.

--- src/System.Text.Primitives/System/Text/Encoders/Utf8Encoder.cs
+++ src/System.Text.Primitives/System/Text/Encoders/Utf8Encoder.cs
@@ -33,6 +33,27 @@ namespace System.Text.Primitives.System.Text.Encoders
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool TryEncodeScalar2(int scalar, Span<byte> output, out int bytesWritten)
+        {
+            // This function can encode code points in the range U+0000..U+10FFFF
+            // excluding the surrogate range U+D800..U+DFFF.
+
+            uint nonNegativeScalar = (uint)scalar; // convert negative numbers to illegally large positive numbers, caught later
+            if (nonNegativeScalar < 0x80U && output.Length != 0)
+            {
+                // Fast case: simple ASCII value with non-empty output buffer.
+                bytesWritten = 1;
+                output.DangerousGetPinnableReference() = (byte)nonNegativeScalar;
+                return true;
+            }
+            else
+            {
+                // Slow case: non-ASCII value *or* invalid value *or* empty output buffer .
+                return TryEncodeScalarCore2(nonNegativeScalar, ref output.DangerousGetPinnableReference(), output.Length, out bytesWritten);
+            }
+        }
+
         private static bool TryEncodeScalarCore(uint scalar, ref byte output, int outputLength, out int bytesWritten)
         {
             // ASCII scalars should be handled by our caller. If an ASCII scalar found its way to
@@ -77,6 +98,57 @@ namespace System.Text.Primitives.System.Text.Encoders
 
             bytesWritten = 0;
             return false;
+        }
+
+        private static bool TryEncodeScalarCore2(uint scalar, ref byte output, int outputLength, out int bytesWritten)
+        {
+            // ASCII scalars should be handled by our caller. If an ASCII scalar found its way to
+            // this method, it's because an empty output buffer was provided, and this will be handled
+            // at the end of the method.
+
+            bytesWritten = 0;
+
+            if ((scalar < 0x800) && (outputLength >= 2))
+            {
+                // write two-byte output [ 110yyyyy 10xxxxxx ]
+
+                output = (byte)(0b1100_0000U | (scalar >> 6));
+                goto WriteScalarRemainder1;
+            }
+            else if ((scalar < 0x10000) && (outputLength >= 3))
+            {
+                if (!Utf8Decoder.IsSurrogate((int)scalar))
+                {
+                    // write three-byte output [ 1110zzzz 10yyyyyy 10xxxxxx ]
+                    output = (byte)(0b1110_0000U | (scalar >> 12));
+                    goto WriteScalarRemainder2;
+                }
+            }
+            else if ((scalar <= 0x10FFFF) && (outputLength >= 4))
+            {
+                // write four-byte output [ 11110uuu 10uuzzzz 10yyyyyy 10xxxxxx ]
+                output = (byte)(0b1111_0000U | (scalar >> 18));
+                goto WriteScalarRemainder3;
+            }
+
+            // If we reached this point, it means one of two things:
+            // a) the output buffer was not large enough to hold the encoded value, or
+            // b) the scalar was outside the ranges U+0000..U+D7FF and U+E000..U+10FFFF.
+            // In either case we fail.
+
+            return false;
+
+            WriteScalarRemainder3:
+            Unsafe.Add(ref output, ++bytesWritten) = (byte)(0b1000_0000U | ((scalar >> 12) & 0b0011_1111U));
+
+            WriteScalarRemainder2:
+            Unsafe.Add(ref output, ++bytesWritten) = (byte)(0b1000_0000U | ((scalar >> 6) & 0b0011_1111U));
+
+            WriteScalarRemainder1:
+            Unsafe.Add(ref output, ++bytesWritten) = (byte)(0b1000_0000U | (scalar & 0b0011_1111U));
+
+            bytesWritten++;
+            return true;
         }
     }
 }
